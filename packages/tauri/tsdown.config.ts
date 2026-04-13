a9857c9740d04f60be9672a7ab0549276b7fae26
Seokju Na <seokju.me@gmail.com>
2026-04-13 23:01:58 +0900

    fix attw

--- packages/tauri/tsdown.config.ts
+++ packages/tauri/tsdown.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig, type UserConfig } from 'tsdown';
 
 const config: UserConfig = defineConfig({
-  // entry: ['./api/index.ts', './api/remote.ts', './api/source.ts', './api/updater.ts'],
   entry: {
     'api/index': './api/index.ts',
-    'api/remote': './api/remote.ts',
-    'api/source': './api/source.ts',
-    'api/update': './api/updater.ts',
+    'api/remote/index': './api/remote.ts',
+    'api/source/index': './api/source.ts',
+    'api/updater/index': './api/updater.ts',
   },
   external: ['@tauri/*'],
   format: ['esm', 'cjs'],
-  platform: 'node',
+  platform: 'neutral',
   target: 'es2020',
   dts: true,
   clean: true,
